4274d8360b1120c03e1eeacb72f41764a771071b
derekacosta <da606@georgetown.edu>
2019-10-21 15:23:13 -0400

    Removed node_module folder

--- node_modules/svg2pdf/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require('./defaultConfig');
-const preflightChecks = require('./preflightChecks');
-const commandParser = require('./commandParser');
-const svg2pdf = require('./svg2pdf');
-
-exports.preflightChecks = preflightChecks;
-exports.defaultConfig = defaultConfig;
-exports.commandParser = commandParser;
-exports.svg2pdf = svg2pdf;
